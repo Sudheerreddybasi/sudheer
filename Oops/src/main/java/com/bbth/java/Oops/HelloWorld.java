99067691afcbe962052aee840309ade26f5431bc
JITHENDRA CHOWDHARY <JITHENDRA CHOWDHARY@jithendra>
2018-03-23 16:16:41 +0530

    Added HelloWorld.java class

---
+++ Oops/src/main/java/com/bbth/java/Oops/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.bbth.java.Oops;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+
+}
